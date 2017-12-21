59c80f956b8caafc3e1339a7386b22c825ec08cc
Sam Churney <sam.churney@redspace.com>
2017-12-21 12:52:25 -0400

    Create basic home and podcasts components; add react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import './App.css';
+import Home from './containers/Home/Home';
+import Podcasts from './containers/Podcasts/Podcasts';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <Route exact path="/" component={Home}/>
+            <Route path="/podcasts" component={Podcasts}/>
+            <hr/>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/podcasts">Podcasts</Link></li>
+            </ul>
+          </div>
+        </Router>
       </div>
     );
   }
